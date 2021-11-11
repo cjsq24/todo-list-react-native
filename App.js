e6e18621b6e28f3cc91944c81880144909c08200
Carlos Sosa <cjsq24@gmail.com>
2021-11-11 09:48:14 -0400

    correccion app, borrando componente de native base

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { NativeBaseProvider } from 'native-base';
 import Todo from './src/views/todo/Todo'
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Todo />
-    </NativeBaseProvider>
+    <Todo />
   );
 }
